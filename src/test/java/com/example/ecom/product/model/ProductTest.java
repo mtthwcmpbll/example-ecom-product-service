d834170d1695286159c9807156bbb0cdfbdf3812
Matt Campbell <matt@moderne.io>
2026-04-07 20:16:50 -0400

    factory: run_recipe

--- src/test/java/com/example/ecom/product/model/ProductTest.java
+++ src/test/java/com/example/ecom/product/model/ProductTest.java
@@ -1,8 +1,8 @@
 package com.example.ecom.product.model;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductTest {
 
